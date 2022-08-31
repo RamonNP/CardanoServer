06b431032883f6066f53216cf15171c7305b9b0a
Ramon <ramon.pereira3156@gmail.com>
2022-08-30 21:02:47 -0300

    adicionado multiplayer
    adicionado multiplayer

--- index.js
+++ index.js
@@ -8,88 +8,83 @@ var io = require('socket.io')(http);
 
 var clientLookup = {};
 
-io.on('connection', function(socket){
+var current_player;
+io.on('connect', function (socket) {
+    socket.on("JOIN_ROOM", function (pack) {
+        console.log(pack.name);
+        current_player = {
+            name: pack.name,
+            id: socket.id,
+            position:'{"position":{"x":"0","y":"2"}}'
+        };
+        console.log("player: " + socket.id + " joined room.");
 
-  var current_player;
+        clientLookup[current_player.id] = current_player;
+        
+        pack = {
+            mEvent: '{"meta":{"event":"connection","actorid":"'+socket.id +'"},"resource":'+current_player.position+'}'
+        };
+        socket.emit("JOIN_SUCCESS",pack);
 
-  socket.on("PING",function(pack){
+        packSombra = {
+            mEvent: '{"meta":{"event":"movimentation","actorid":"'+socket.id +'"},"resource":'+current_player.position+'}'
+        };
 
+        //Envia sombras
+        socket.broadcast.emit("JOIN_SUCCESS",packSombra);
+        //agora enviar TODOS os jogadores para o jogador atual
+        for (client in clientLookup) {
+            if (clientLookup[client].id != current_player.id) {
+                pack = {
+                    mEvent: '{"meta":{"event":"movimentation","actorid":"'+clientLookup[client].id+'"},"resource":'+clientLookup[client].position+'}'
+                };
+                //socket.emit('SPAWN_PLAYER', clientLookup[client]);
+                socket.emit("JOIN_SUCCESS",pack);
+            } 
+        }
+        //enviar play local
+        console.log(pack);
+    });//END_SOCKET.ON
 
-   console.log('menssagem recebida do unity: '+pack.message);
+    socket.on("MOVE_AND_ROT", function (pack) {
+        console.log(pack);
+        lastPlayerPosition = JSON.parse(pack);
+        clientLookup[lastPlayerPosition["meta"]["actorid"]].position = JSON.stringify(lastPlayerPosition["resource"]);
 
-   var json_pack = {
+        var data = {
+            mEvent:pack
+        };
+        socket.broadcast.emit('UPDATE_POS_ROT', data);
+        //socket.emit('UPDATE_POS_ROT', data);
+    });//END_SOCKET.ON
 
-     message:"pong!!!"
+    socket.on('ANIMATION', function (pack) {
+        console.log(pack);
+        var data = {
+            mEvent:pack
+        };
+        socket.broadcast.emit('UPDATE_ANIMATOR', data);
 
-   };
+    });//END_SOCKET.ON
 
-  socket.emit("PONG",json_pack);
-
-
-});//END_SOCKET.ON
-
-socket.on("JOIN_ROOM",function(pack){
-
-  current_player = {
-
-    name : pack.name,
-    id: socket.id
-  };
-  console.log('Player Conectado!'+pack.name);
- clientLookup[current_player.id] = current_player;
-
- socket.emit("JOIN_SUCCESS",current_player);
-
- //envia o jogador atual para TODOS  os jogadores online
- socket.broadcast.emit('SPAWN_PLAYER',current_player);
-
- //agora enviar TODOS os jogadores para o jogador atual
- for(client in clientLookup)
- {
-   if(clientLookup[client].id!=current_player.id)
-   {
-     socket.emit('SPAWN_PLAYER',clientLookup[client]);
-   }
- }
-
-
-});//END_SOCKET.ON
-
-socket.on("MOVE_AND_ROT",function(pack){
-
- var data = {
-   id:current_player.id,
-   position:pack.position,
-   rotation:pack.rotation,
-   mov:pack.mov
- };
-
- socket.broadcast.emit('UPDATE_POS_ROT',data);
-
-});//END_SOCKET.ON
-
-socket.on('ANIMATION',function(pack){
-
-  socket.broadcast.emit('UPDATE_ANIMATOR',{id:pack.id,
-                                            animation:pack.animation});
-
-});//END_SOCKET.ON
-
-socket.on('disconnect',function(){
-
- socket.broadcast.emit('USER_DISCONNECTED',{id:current_player.id});
-
- delete clientLookup[current_player.id];
- 
-
-});//END_SOCKET.ON
+    socket.on('disconnect', function () {        
+        //Talvez não emit Disconect, fazer rotina de vericar de tempos em tempos
+        delete clientLookup[socket.id];
+        
+        pack = {
+            mEvent: '{"meta":{"event":"disconnection","actorid":"'+socket.id+'"},"resource":{"position":{"x":"0","y":"2"}}}'
+        };
+        
+        socket.broadcast.emit('USER_DISCONNECTED',pack);
+        console.log('DESCONECTOU ZÈ');
+    });//END_SOCKET.ON
 
 });//END_IO.ON
 
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(3045, function () {
 
-console.log('server listen on 3000!'+process.env.PORT);
+    console.log('server listen on 3045!');
 
 });
 
